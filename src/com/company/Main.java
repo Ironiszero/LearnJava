e3ec76bb064f1c09edda4feb155b464e76efe38b
Ironiszero <2320262938@qq.com>
2021-07-13 15:15:26 +0800

    equals方法

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,46 @@
 package com.company;
-class Apple extends Object{
+class Goat{
     private String color;
     private double weight;
-    public Apple(){
+    public Goat(){
+
     }
-    public Apple(String color,double weight){
+    public Goat(String color,double weight){
         this.color=color;
         this.weight=weight;
     }
     public void setColor(String color){
         this.color=color;
     }
-    @Override
-    public String toString(){
-        return "Apple[color]=color"+color+",weight="+weight+"]";//列出所有的成员变量；
+    public String getColor(){
+        return this.color;
     }
+    public void setWeight(double weight){
+        this.weight=weight;
+    }
+    public double getWeight(){
+        return this.weight;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if(this==obj){
+            return true;
+        }
+        if(obj!=null&&obj.getClass()==Goat.class){
+            Goat target=(Goat)obj;
+            return this.color.equals(color)&&this.weight==weight;
+        }
+        return false;
+    }
+
 }
-public class Main {
+public class Main{
     public static void main(String[] args){
-        Apple ap=new Apple("红色",2.3);
-//        下面两行代码完全相同
-        System.out.println(ap);
-        System.out.println(ap.toString());
-        Apple ap2=new Apple("lv",1.7);
-        System.out.println(ap2);
-//        String str=ap2;
-//        任何对象加上”“，就会变成字符串
-        String str=ap2+"";
-        System.out.println(str);
+        Goat goat1=new Goat("黑色",78.2);
+        Goat goat2=new Goat("黑色",78.2);
+        System.out.println(goat1==goat2);
+//        goat1与goat2分别指向两个不同的对象，因此==返回false；
+        System.out.println(goat1.equals(goat2));
+
     }
 }
