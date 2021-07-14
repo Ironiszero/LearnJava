23a35a9adf8259eaf15771ef4dcd968a24ec07d7
Ironiszero <2320262938@qq.com>
2021-07-14 16:13:15 +0800

    final修饰引用类型变量

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,39 @@
 package com.company;
-public class Main{
-    class A{
+class Pig {
+    private String color;
+    private double weight;
+    public Pig(){
 
     }
+    public Pig(String color,double weight){
 
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getWeight(String color) {
+        return color;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+    @Override
+    public String toString(){
+        return "Pig[color="+this.color+",weight="+this.weight+"]";
+    }
+}
+public class Main {
     public static void main(String[] args){
-
-//        A a=new A();
-//        静态方法使用了非静态的内部类；
-
+        final Pig p=new Pig("小猪",88);
+        p.setColor("黑色");
+        p.setWeight(44.5);
+        System.out.println(p);
     }
+
 }
