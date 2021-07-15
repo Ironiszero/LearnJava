8fda57cea18d64be959e32e7723d4098b3cb731d
Ironiszero <2320262938@qq.com>
2021-07-15 11:02:36 +0800

    final修饰变量

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,25 @@
 package com.company;
-class Pig {
-    private String color;
-    private double weight;
-    public Pig(){
-
-    }
-    public Pig(String color,double weight){
-
-    }
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getWeight(String color) {
-        return color;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-    @Override
-    public String toString(){
-        return "Pig[color="+this.color+",weight="+this.weight+"]";
-    }
-}
-public class Main {
+public class Main{
     public static void main(String[] args){
-        final Pig p=new Pig("小猪",88);
-        p.setColor("黑色");
-        p.setWeight(44.5);
-        System.out.println(p);
+//        有final修饰，指定了初始值，即初始值在编译的时候就可以确定
+        final int MAX=100;
+        System.out.println(MAX);
+        String str1="fkjava";
+        String str2="fkjava";
+        System.out.println(str1==str2);
+        String str3="fkit.org";
+        String str4="fkit"+".org";
+        System.out.println(str3==str4);
+        final String str5="fkit";
+        final String str6=".org";
+        String str7=str5+str6;
+        System.out.println(str7==str3);
+//        下面代码有几个变量，几个对象？0个变量，1个对象；
+        final String s1="fkit";
+        final String s2=s1+".org";
+        final String s3=s2+" is ";
+        final String s4=s3+" a very";
+        final String s5=s4+" good education center!";
+        System.out.println(s5);
     }
-
 }
